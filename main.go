98f7aed9093261d0d9152e8458391dc2e88188ee
Mark Hollands <markjhollands@gmail.com>
2019-10-15 20:19:16 +0100

    Change updateName() function to use person pointer

--- main.go
+++ main.go
@@ -23,7 +23,13 @@ func main() {
 		},
 	}
 
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
 	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
 
 func (p person) print() {
